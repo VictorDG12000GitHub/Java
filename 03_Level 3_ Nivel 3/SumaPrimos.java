36ebd6af65c08bd6e6d66dca35f3bf5cdcb0df11
VictorDG12000GitHub <victorgithub12000@gmail.com>
2024-04-23 10:32:10 +0200

    primos

--- 03_Level 3_ Nivel 3/SumaPrimos.java
+++ 03_Level 3_ Nivel 3/SumaPrimos.java
@@ -1,5 +1,20 @@
 public class Main{
     public static void main(String args[]){
-        
+        int cont=0;
+        for(int i=1;i<101;i++){
+            System.out.print(i);
+            cont=0;
+            for(int j=i-1;j>2;j--){
+                if(i%j==0){
+                    cont++;
+                     
+                }
+
+            }
+
+            if(cont==0){
+                System.out.print("Primo.");
+            }
+        }
     }
 }
